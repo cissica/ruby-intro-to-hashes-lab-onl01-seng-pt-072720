9d8e9e93ad64b1f41ba7a25893bad1894933e640
Cissica Gonzalez Martinez <cecemartinez@hotmail.com>
2020-08-10 07:20:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,6 +40,12 @@ def monopoly_with_third_tier
     :names=>
     {
       :reading_railroad=>
+      {},
+      :pennsylvania_railroad=>
+      {},
+      :b_and_o_railroad=>
+      {},
+      :shortline=>
       {}
   }
 }#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
